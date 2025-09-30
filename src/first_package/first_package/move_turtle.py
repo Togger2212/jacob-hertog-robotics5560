0846bf9d96ba5b2e81f8add543ea920bca3713da
Togger2212 <jacobhertog@gmail.com>
2025-09-30 14:51:57 -0400

    final-change

--- src/first_package/first_package/move_turtle.py
+++ src/first_package/first_package/move_turtle.py
@@ -14,10 +14,13 @@ class TurtleSwim(Node):
         timer_period = 0.01  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.last_state = 0
+        self.total_state = 0
         self.state = 'forward'
         self.vel = Twist()
 
     def timer_callback(self):
+        if self.total_state >= 2400:
+            self.stop()
         if self.state =='forward':
             if(self.last_state > 500):
                 self.state = 'turn'
@@ -32,8 +35,9 @@ class TurtleSwim(Node):
                 self.last_state = 0
             else:
                 self.rotate()
-                
+
         self.last_state+=1
+        self.total_state +=1
         self.publisher_.publish(self.vel)
 
         
@@ -49,7 +53,10 @@ class TurtleSwim(Node):
     def rotate(self):
         self.vel.linear.x = 0.0
         self.vel.angular.z = 3.14159/2.0
-        
+
+    def stop(self):
+        self.vel = Twist()
+        rclpy.shutdown()
         
     
 
